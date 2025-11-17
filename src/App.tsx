0a94af48dd86be1d124e5d66cc846ee9342b048a
Sardorbek <sardorbekmusilman@gmail.com>
2025-11-17 17:20:58 +0500

    Add spacing for improved readability in App component routes

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,11 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
 
         {/* Protected routes */}
+
+
+
+
+        
         <Route
           path="/*"
           element={
